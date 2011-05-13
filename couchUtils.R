eb3a211801e990df5fa347d39347e7a38634d30b
James E. Marca <jmarca@translab.its.uci.edu>
2011-05-12 21:16:55 -0700

    fixed couchUtils.  can now create db, get put, etc
    Much more robust usage of couchdb to store data
     modified:   couchUtils.R
    
    Not yet ready to run this.  Small changes to properly set up the couchUtils file
     modified:   processPems.R

--- couchUtils.R
+++ couchUtils.R
@@ -1,30 +1,51 @@
+## requires that dbname be set externally
+couchenv = Sys.getenv(c("COUCHDB_HOST", "COUCHDB_USER", "COUCHDB_PASS"))
+couchdb = paste("http://",couchenv[1],":5984",sep='')
 
-couch.get <- function(district,year,vdsid){
 
-  docname = vdsid
-  restful = paste(dbname,district,year,sep='%2F')
-  uri=paste(couchdb,restful,docname,sep="/");
+couch.makedbname <- function( components ){
+  tolower(paste(components,collapse='%2F'))
+}
+
+
+couch.makedb <- function( db ){
+
+  if(length(db)>0){
+    db <- couch.makedbname(db)
+  }
+
+  privcouchdb = paste("http://",couchenv[2],":",couchenv[3],"@",couchenv[1],":5984",sep='')
+  uri=paste(privcouchdb,db,sep="/");
+
+  reader = basicTextGatherer()
+
+  curlPerform(
+              url = uri
+              ,httpheader = c('Content-Type'='application/json')
+              ,customrequest = "PUT"
+              ,writefunction = reader$update
+              )
+
+  reader$value()
+}
+
+couch.get <- function(db,doc){
+
+  if(length(db)>0){
+    db <- couch.makedbname(db)
+  }
+  uri=paste(couchdb,db,doc,sep="/");
   fromJSON(getURL(uri)[[1]])
 
 }
 
+couch.put <- function(db,docname,doc){
 
-couch.check.is.processed <- function(district,year,vdsid){
-
-  statusdoc = couch.get(district,year,vdsid)
-  result <- FALSE ## doc needs to be processed
-  fieldcheck <- c('error','processed') %in% names(statusdoc)
-  if(!fieldcheck[1] && fieldcheck[2]){
-    result <- TRUE
+  if(length(db)>0){
+    db <- couch.makedbname(db)
   }
-  result
 
-}
-
-couch.put <- function(district,year,vdsid,doc){
-
-  restful = paste(dbname,district,year,sep='%2F')
-  uri=paste(couchdb,restful,vdsid,sep="/");
+  uri=paste(couchdb,db,docname,sep="/");
 
   reader = basicTextGatherer()
 
@@ -39,12 +60,28 @@ couch.put <- function(district,year,vdsid,doc){
   reader$value()
 }
 
+
+couch.check.is.processed <- function(district,year,vdsid){
+
+  statusdoc = couch.get(c(district,year),vdsid)
+  result <- TRUE ## default to done
+  fieldcheck <- c('error','inprocess','processed') %in% names(statusdoc)
+  if(fieldcheck[1] && !fieldcheck[2] && !fieldcheck[3]){
+    putstatus <- fromJSON(couch.put(c(district,year),vdsid,list(inprocess=1)))
+    fieldcheck <- c('error') %in% names(putstatus)
+    if(!fieldcheck[1]){
+      result <- FALSE
+    }
+  }
+  result
+
+}
+
+
 couch.save.is.processed <- function(district,year,vdsid,doc=list(processed=1)){
 
-  current = couch.get(district,year,vdsid)
-  if( c('_rev') %in% names(current) ){
-    doc = merge (current,doc)
-  }
-  couch.put(district,year,vdsid,doc)
+  current = couch.get(c(district,year),vdsid)
+  doc = merge (current,doc)
+  couch.put(c(district,year),vdsid,doc)
 
 }
